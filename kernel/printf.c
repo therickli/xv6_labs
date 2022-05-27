5a36ce8a8b0b694814c1fd610ecf6119001ade55
limiao <therickli@gmail.com>
2022-05-27 22:48:30 +0800

    i

--- kernel/printf.c
+++ kernel/printf.c
@@ -132,3 +132,16 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void
+backtrace(void)
+{
+  uint64 fp = r_fp();
+  uint64 top = PGROUNDUP(fp);
+  printf("backtrace:\n");
+  while (fp < top)
+  {
+    printf("%p\n", *(uint64 *)(fp - 8));
+    fp = *(uint64 *)(fp - 16);
+  }
+}
